363139c181896da07591a2b79731fd0c523b7419
JoeltonLP <joelton.mpto.dev@gmail.com>
2021-04-10 14:38:37 -0300

    circlo_area_v7

--- circle_area.py
+++ circle_area.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 # Um pouco sobre módulo.
-
 from math import pi
 
 # Adicionando uma entrada do usário.
+if __name__ == '__main__':
+    raio = input('Raio: ')
+    circlo_area = pi * float(raio) ** 2
+    print(circlo_area)
 
-raio = input('Raio: ')
-circlo_area = pi * float(raio) ** 2
-
-print('Module name: ', __name__)
-print('Package name: ', __package__)
+print(dir('__main__'))
